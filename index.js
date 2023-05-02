d015d3203b2c3fe8acab2743826ea155bc03618c
Mahfuz <aamahfuz.pro@gmail.com>
2023-05-02 17:03:59 +0600

    chefdata done

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
+const chefs = require("./Data/Chefdata.json");
 app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("Unique is running");
+});
+
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
 });
 
 app.listen(port, () => {
