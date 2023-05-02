26781ce130129e90589ec8929a5b88b4fa08f498
Mahfuz <aamahfuz.pro@gmail.com>
2023-05-02 22:12:05 +0600

    recipe data created done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 const chefs = require("./Data/Chefdata.json");
+const recipes = require("./Data/Recipedata.json");
 app.use(cors());
 
 app.get("/", (req, res) => {
@@ -13,7 +14,15 @@ app.get("/", (req, res) => {
 app.get("/chefs", (req, res) => {
   res.send(chefs);
 });
-
+app.get("/recipes", (req, res) => {
+  res.send(recipes);
+});
+app.get("/recipes/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const selectedRecipe = recipes.find((r) => r.id == id);
+  res.send(selectedRecipe);
+});
 app.listen(port, () => {
   console.log(`unique is running on port: ${port}`);
 });
